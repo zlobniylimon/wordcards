58c78eb7e3ad7ec8a470078f3599e1937f20371f
unknown <zlobniy.limon@gmail.com>
2021-03-18 03:15:55 +0300

    remove secret key from repository files

--- wordcards/settings.py
+++ wordcards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k$4r2fcc=nm--2#s%p1g2c=y@e^$fvb61chf%bh_4$e#ijj8n='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
